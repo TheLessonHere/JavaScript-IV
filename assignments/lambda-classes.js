07f8f1371e99e38686bb9fcb4d485e2a3a1d219a
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-06-13 14:12:54 -0600

    Lambda-Classes Person-Student classes created

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,45 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(properties) {
+        this.name = properties.name;
+        this.age = properties.name;
+        this.location = properties.location;
+    };
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    };
+};
+
+class Instructor extends Person {
+    constructor(properties) {
+        super(properties);
+        this.specialty = properties.specialty;
+        this.favLanguage = properties.favLanguage;
+        this.catchPhrase = properties.catchPhrase;
+    };
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    };
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    };
+};
+
+class Student extends Person {
+    constructor(properties) {
+        super(properties);
+        this.background = properties.background;
+        this.className = properties.className;
+        this.favSubjects = properties.favSubjects;
+    };
+    listsSubjects() {
+        console.log(...this.favSubjects);
+    };
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`);
+    };
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
+    };
+}
